dfddde8e39c9fd99bf9c95118fca4c165b6d1fdf
Arodriguez1030 <arodriguez1030@gmail.com>
2021-10-28 11:59:22 -0400

    set up category reducer, passed it to the store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import categoryReducer from './reducers/categoryReducer';
 
 import App from './App';
 
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+let store = createStore(categoryReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
