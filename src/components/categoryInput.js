9e75055db01af300577385d89eaa35ce4501eddc
Arodriguez1030 <arodriguez1030@gmail.com>
2021-11-07 06:57:15 -0500

    created form page for new category

---
+++ src/components/categoryInput.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class CategoryInput extends Component {
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <label>New category name:</label>
+                    <input></input>
+                </form>
+            </div>
+        )
+    }
+}
