49e79d367802654305bf99c70dca156e01f27653
Arodriguez1030 <arodriguez1030@gmail.com>
2021-11-14 21:56:41 -0500

    added video component and video reducer

--- src/actions/fetchVideos.js
+++ src/actions/fetchVideos.js
@@ -1,11 +1,11 @@
-export const fetchCategories = () => {
+export const fetchvideos = () => {
    
     return (dispatch) => {
-        dispatch({type: "LOADING_CATEGORIES"});
+        dispatch({type: "LOADING_VIDEOS"});
         fetch("http://localhost:3000/api/v2/videos")
         .then(resp => resp.json())
         .then(response => {
-            dispatch({type: "FETCH_CATEGORIES", payload: response.data})
+            dispatch({type: "FETCH_VIDEOS", payload: response.data})
         })
     }
 
