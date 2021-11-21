bb100266c867074868e1f99da23ce3aeae7721cf
Arodriguez1030 <arodriguez1030@gmail.com>
2021-11-21 14:02:10 -0500

    included video input and entered form within videos container

---
+++ src/components/videoInput.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import { connect } from "react-redux";
+
+class VideoInput extends Component {
+
+    render(){
+        render(
+            <div>VideoInput</div>
+        )
+    }
+}
+
+export default VideoInput
--- src/containers/videosContainer.js
+++ src/containers/videosContainer.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import VideoInput from "../components/videoInput";
 
 class VideosContainer extends Component {
     render() {
         return(
-            <div>videos</div>
+            <div><VideoInput/></div>
         )
     }
 }
